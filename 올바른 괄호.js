54581d63a7da1ad47b744e8edabf93de9154c2ce
KoreanCodingMachine <pon03078@naver.com>
2022-07-08 03:44:53 +0900

    lv2 첫 문제풀이

---
+++ 올바른 괄호.js
@@ -0,0 +1,44 @@
+// 괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+
+// "()()" 또는 "(())()" 는 올바른 괄호입니다.
+// ")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+// '(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때,
+// 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+
+// 제한사항
+// 문자열 s의 길이 : 100,000 이하의 자연수
+// 문자열 s는 '(' 또는 ')' 로만 이루어져 있습니다.
+
+// 입출력 예
+// s	answer
+// "()()"	true
+// "(())()"	true
+// ")()("	false
+// "(()("	false
+// '())('
+
+function solution(s) {
+  let answer = true;
+  let left_count = 0;
+
+  if (s[0] === ')') {
+    return (answer = false);
+  } else if (s.length % 2 === 1) {
+    return (answer = false);
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      left_count++;
+    } else if (left_count >= 1 && s[i] === ')') {
+      left_count--;
+    }
+  }
+
+  if (left_count === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(solution('()()'));
